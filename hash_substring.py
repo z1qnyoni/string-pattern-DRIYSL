437068b2affac6043b40ec1a1c76a9daead0fd5e
DANIELS RAIVO IVANOVS 211RMB021 <118028746+DRIYSL@users.noreply.github.com>
2023-04-16 06:23:26 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -6,24 +6,21 @@ def read_input():
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     input_type = input().strip().upper()
     pattern = input().strip()
+    text = input().strip()
 
-    
-    if input_type == 'I':
-        pattern = input().strip()
-        text = input().strip()
-        return (input_type, pattern, text)
 
-    elif input_type == "F":
+    if input_type == "F":
         file_name = pattern
         with open(file_name, 'r') as file:
             text = file.read().strip()
-            return (input_type, pattern, text)
+    elif input_type == "I":
+        text = input().strip()
+     
     else:
         print("Invalid character, please try again")
         exit()
-
-   
-
+    return(input_type, pattern, text)
+    
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
